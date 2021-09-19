d4669ed8e368e0562f353cae6c596c43771a2478
Michael Belousov <mike.belousov@bentley.com>
2021-09-18 21:15:01 -0400

    finish bisect loop, learn there's definitely some stupid logic in the hash

--- dist_test.py
+++ dist_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 # from collections import OrderedDict as odict
+import os
 import pickle
+from bisect import bisect_left
 
 bit_widths = {'z':1,'j':1,'q':1,'x':1,'k':1,'v':1,'b':1,'p':1,'g':1,'w':1,'y':1,'f':1,'m':2,'c':2,'u':2,'l':3,'d':3,'h':4,'r':4,'s':4,'n':4,'i':4,'o':4,'a':4,'t':5,'e':7}
 
@@ -27,10 +29,15 @@ def hash_all_words():
         reverse.setdefault(v, []).append(k)
     return words, reverse
 
-import os
-import pickle
-
 PICKLE_PATH = os.path.join(os.getcwd(), '.wordcache')
+
+def join_iter(iterable, delimiter):
+    """aka intersperse"""
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
 
 if __name__ == '__main__':
     if os.path.exists(PICKLE_PATH):
@@ -38,13 +45,21 @@ if __name__ == '__main__':
             from_word, from_hash, sorted_hashes = pickle.loads(pickle_file.read())
     else:
         from_word, from_hash = hash_all_words()
-        sorted_hashes = sorted(from_hash)
+        sorted_hashes = list(sorted(from_hash))
         with open(PICKLE_PATH, 'xb') as pickle_file:
             pickle_file.write(pickle.dumps((from_word, from_hash, sorted_hashes)))
 
     while True:
         try:
-            word = input("enter a word:")
+            word = input("enter a word> ")
+            if word in from_word:
+                index = bisect_left(sorted_hashes, from_word[word], 0, len(sorted_hashes))
+                collisions = from_hash[from_word[word]]
+                print("matches for that one word:", *(k for k,_ in zip(join_iter(collisions, ';'), range(10))), f"; and {len(collisions) - 10} more..." if len(collisions) > 10 else "" )
+                print("hash for that word:", from_word[word])
+            else:
+                print("word was not in the corpus")
         except KeyboardInterrupt:
+            print('interrupt, exiting')
             break
 
